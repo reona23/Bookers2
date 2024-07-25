61e06d1f0ed6d3013506b4b278ca3562234d23f3
EC2 Default User <ec2-user@ip-172-31-14-116.ap-northeast-1.compute.internal>
2024-07-25 05:47:44 +0000

    [Finish] file setup

---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ApplicationRecord
+
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :body, presence: true
+
+  has_one_attached :image
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "homes#top"
+  get 'about', to: 'homes#about'
+
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books
 end
