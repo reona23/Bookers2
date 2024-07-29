7566af21b880310908cd11f47fb5259c03cb34d6
EC2 Default User <ec2-user@ip-172-31-14-116.ap-northeast-1.compute.internal>
2024-07-29 04:50:19 +0000

    [Fin} フラッシュメッセージの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
 
   validates :name, presence: true, uniqueness: true
 
   has_many :books, dependent: :destroy
+
+  has_one_attached :image
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get 'about', to: 'homes#about'
+  get 'homes/about', to: 'homes#about', as: 'about'
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books
+  resources :books, only: [:index, :show, :edit, :update, :create, :new, :destroy]
 end
